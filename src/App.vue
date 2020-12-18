eab5086b246959edc818b8943513ca199ca0941e
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-12-18 14:59:47 +0100

    Initial setups

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Home from './pages/Home.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Home,
   },
 };
 </script>
 
 <style lang="scss">
+@import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css');
+@import url('https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css');
+
+@font-face {
+  font-family: "MTN-sans";
+  src: url(./assets/fonts/MTNBrighterSans-Regular.ttf);
+}
+
+@font-face {
+  font-family: "MTN-sans";
+  src: url(./assets/fonts/MTNBrighterSans-Medium.ttf);
+  font-weight: 600;
+}
+
+@font-face {
+  font-family: "MTN-sans";
+  src: url(./assets/fonts/MTNBrighterSans-Bold.ttf);
+  font-weight: 700;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: MTN-sans, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Nav />
+    Hello Home
+  </div>
+</template>
+
+<script>
+import Nav from '@/components/Nav.vue';
+
+export default {
+  components: {
+    Nav,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
