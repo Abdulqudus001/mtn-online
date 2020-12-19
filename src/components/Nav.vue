15e87ef0ca05d3c8f941e15bf4683a9406a33f63
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-12-19 11:26:49 +0100

    Added giphy search

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -3,7 +3,7 @@
     <ul class="row align-items-center mx-0">
       <li class="navbar__logo ml-0">
         <a href="/">
-          <img src="../assets/logo.svg" alt="MTNNG logo image">
+          <img src="../assets/logo.svg" alt="MTNNG logo image" />
         </a>
       </li>
       <div
@@ -25,42 +25,34 @@
               @mouseover="openSubMenu('services')"
               @mouseleave="openSubMenu('')"
             >
-              <a href="#" class="mx-0 row justify-content-between align-items-center flex-nowrap">
-                <span>
-                  Services
-                </span>
+              <a
+                href="#"
+                class="mx-0 row justify-content-between align-items-center flex-nowrap"
+              >
+                <span> Services </span>
                 <i class="fa fa-angle-right ml-4"></i>
               </a>
-              <ul v-if="currentSubMenu === 'services'" class="sub-menu sub-menu--sub">
+              <ul
+                v-if="currentSubMenu === 'services'"
+                class="sub-menu sub-menu--sub"
+              >
                 <li class="mx-0">
-                  <a href="#">
-                    Tariff Plans
-                  </a>
+                  <a href="#"> Tariff Plans </a>
                 </li>
                 <li class="mx-0">
-                  <a href="#">
-                    Roaming
-                  </a>
+                  <a href="#"> Roaming </a>
                 </li>
                 <li class="mx-0">
-                  <a href="#">
-                    International Rates
-                  </a>
+                  <a href="#"> International Rates </a>
                 </li>
                 <li class="mx-0">
-                  <a href="#">
-                    Country Code Prefix
-                  </a>
+                  <a href="#"> Country Code Prefix </a>
                 </li>
                 <li class="mx-0">
-                  <a href="#">
-                    Recharge
-                  </a>
+                  <a href="#"> Recharge </a>
                 </li>
                 <li class="mx-0">
-                  <a href="#">
-                    SIM Services
-                  </a>
+                  <a href="#"> SIM Services </a>
                 </li>
               </ul>
             </li>
@@ -69,32 +61,28 @@
               @mouseover="openSubMenu('data')"
               @mouseleave="openSubMenu('')"
             >
-              <a href="#" class="mx-0 row justify-content-between align-items-center flex-nowrap">
-                <span>
-                  Data
-                </span>
+              <a
+                href="#"
+                class="mx-0 row justify-content-between align-items-center flex-nowrap"
+              >
+                <span> Data </span>
                 <i class="fa fa-angle-right ml-4"></i>
               </a>
-              <ul v-if="currentSubMenu === 'data'" class="sub-menu sub-menu--sub">
+              <ul
+                v-if="currentSubMenu === 'data'"
+                class="sub-menu sub-menu--sub"
+              >
                 <li class="mx-0">
-                  <a href="#">
-                    Data Plans
-                  </a>
+                  <a href="#"> Data Plans </a>
                 </li>
                 <li class="mx-0">
-                  <a href="#">
-                    Social Media Bundles
-                  </a>
+                  <a href="#"> Social Media Bundles </a>
                 </li>
                 <li class="mx-0">
-                  <a href="#">
-                    Video Streaming Pack
-                  </a>
+                  <a href="#"> Video Streaming Pack </a>
                 </li>
                 <li class="mx-0">
-                  <a href="#">
-                    Data Gifting
-                  </a>
+                  <a href="#"> Data Gifting </a>
                 </li>
               </ul>
             </li>
@@ -103,27 +91,25 @@
               @mouseover="openSubMenu('devices')"
               @mouseleave="openSubMenu('')"
             >
-              <a href="#" class="mx-0 row justify-content-between align-items-center flex-nowrap">
-                <span>
-                  Devices
-                </span>
+              <a
+                href="#"
+                class="mx-0 row justify-content-between align-items-center flex-nowrap"
+              >
+                <span> Devices </span>
                 <i class="fa fa-angle-right ml-4"></i>
               </a>
-              <ul v-if="currentSubMenu === 'devices'" class="sub-menu sub-menu--sub">
+              <ul
+                v-if="currentSubMenu === 'devices'"
+                class="sub-menu sub-menu--sub"
+              >
                 <li class="mx-0">
-                  <a href="#">
-                    Device Financing
-                  </a>
+                  <a href="#"> Device Financing </a>
                 </li>
                 <li class="mx-0">
-                  <a href="#">
-                    4G MiFi
-                  </a>
+                  <a href="#"> 4G MiFi </a>
                 </li>
                 <li class="mx-0">
-                  <a href="#">
-                    Smart Feature Phone
-                  </a>
+                  <a href="#"> Smart Feature Phone </a>
                 </li>
               </ul>
             </li>
@@ -132,39 +118,33 @@
               @mouseover="openSubMenu('started')"
               @mouseleave="openSubMenu('')"
             >
-              <a href="#" class="mx-0 row justify-content-between align-items-center flex-nowrap">
-                <span>
-                  Getting Started
-                </span>
+              <a
+                href="#"
+                class="mx-0 row justify-content-between align-items-center flex-nowrap"
+              >
+                <span> Getting Started </span>
                 <i class="fa fa-angle-right ml-4"></i>
               </a>
-              <ul v-if="currentSubMenu === 'started'" class="sub-menu sub-menu--sub">
+              <ul
+                v-if="currentSubMenu === 'started'"
+                class="sub-menu sub-menu--sub"
+              >
                 <li class="mx-0">
-                  <a href="#">
-                    Coverage Map
-                  </a>
+                  <a href="#"> Coverage Map </a>
                 </li>
                 <li class="mx-0">
-                  <a href="#">
-                    Find a store
-                  </a>
+                  <a href="#"> Find a store </a>
                 </li>
                 <li class="mx-0">
-                  <a href="#">
-                    How-to guides
-                  </a>
+                  <a href="#"> How-to guides </a>
                 </li>
                 <li class="mx-0">
-                  <a href="#">
-                    Internet settings
-                  </a>
+                  <a href="#"> Internet settings </a>
                 </li>
               </ul>
             </li>
             <li class="mx-0">
-              <a href="#">
-                myMTN App
-              </a>
+              <a href="#"> myMTN App </a>
             </li>
           </ul>
         </li>
@@ -182,24 +162,16 @@
           </a>
           <ul v-if="currentMenu === 'investors'" class="sub-menu">
             <li class="mx-0">
-              <a href="#">
-                More in Investors
-              </a>
+              <a href="#"> More in Investors </a>
             </li>
             <li class="mx-0">
-              <a href="#">
-                Shareholders
-              </a>
+              <a href="#"> Shareholders </a>
             </li>
             <li class="mx-0">
-              <a href="#">
-                Financial Reporting
-              </a>
+              <a href="#"> Financial Reporting </a>
             </li>
             <li class="mx-0">
-              <a href="#">
-                Announcements
-              </a>
+              <a href="#"> Announcements </a>
             </li>
           </ul>
         </li>
@@ -220,14 +192,10 @@
           </a>
           <ul v-if="currentMenu === 'support'" class="sub-menu">
             <li class="mx-0">
-              <a href="#">
-                Help Center
-              </a>
+              <a href="#"> Help Center </a>
             </li>
             <li class="mx-0">
-              <a href="#">
-                Community
-              </a>
+              <a href="#"> Community </a>
             </li>
           </ul>
         </li>
@@ -248,23 +216,78 @@
           </a>
           <ul v-if="currentMenu === 'contact'" class="sub-menu">
             <li class="mx-0">
-              <a href="#">
-                MTN Supplier
-              </a>
+              <a href="#"> MTN Supplier </a>
             </li>
           </ul>
         </li>
         <li class="d-lg-none">
           <div class="search-bar">
             <i class="fa fa-search"></i>
-            <input type="search" placeholder="Search...">
+            <i class="fa fa-search"></i>
+            <input
+              type="search"
+              placeholder="Search..."
+              @keyup="search"
+              v-model="searchText"
+            />
+            <div v-if="showRes" class="results">
+              <button
+                @click="
+                  showRes = false;
+                  searchText = '';
+                "
+              >
+                <i class="fa fa-times"></i>
+              </button>
+              <template v-if="loading">
+                <div class="loader"></div>
+              </template>
+              <template v-if="loading === false && !errorText">
+                <div class="gifs">
+                  <div v-for="gif in gifs" :key="gif.id" class="gif">
+                    <img :src="gif.images.preview_gif.url" alt="" />
+                  </div>
+                </div>
+              </template>
+              <template v-else>
+                <p class="error">{{ errorText }}</p>
+              </template>
+            </div>
           </div>
         </li>
       </div>
     </ul>
     <div class="search-bar d-none d-lg-block">
       <i class="fa fa-search"></i>
-      <input type="search" placeholder="Search...">
+      <input
+        type="search"
+        placeholder="Search..."
+        @keyup="search"
+        v-model="searchText"
+      />
+      <div v-if="showRes" class="results">
+        <button
+          @click="
+            showRes = false;
+            searchText = '';
+          "
+        >
+          <i class="fa fa-times"></i>
+        </button>
+        <template v-if="loading">
+          <div class="loader"></div>
+        </template>
+        <template v-if="loading === false && !errorText">
+          <div class="gifs">
+            <div v-for="gif in gifs" :key="gif.id" class="gif">
+              <img :src="gif.images.preview_gif.url" alt="" />
+            </div>
+          </div>
+        </template>
+        <template v-else>
+          <p class="error">{{ errorText }}</p>
+        </template>
+      </div>
     </div>
     <button
       class="hamburger d-lg-none"
@@ -279,11 +302,19 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: () => ({
     active: false,
     currentMenu: '',
     currentSubMenu: '',
+    searchText: '',
+    showRes: false,
+    loading: false,
+    timeout: null,
+    errorText: '',
+    gifs: [],
   }),
   methods: {
     openMenuClick(name) {
@@ -305,13 +336,54 @@ export default {
         this.currentSubMenu = name;
       }
     },
+    handleSearch() {
+      this.showRes = true;
+      this.loading = true;
+      this.errorText = '';
+      const token = process.env.VUE_APP_TOKEN;
+      axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${token}&q=${this.searchText}`)
+        .then((res) => {
+          this.loading = false;
+          this.gifs = res.data.data;
+        })
+        .catch((err) => {
+          this.loading = false;
+          console.log(err);
+          this.errorText = 'Something went wrong, check your network and try again';
+        });
+    },
+    search() {
+      if (this.timeout) {
+        clearTimeout(this.timeout);
+      }
+
+      this.timeout = setTimeout(this.handleSearch, 200);
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+}
+.loader {
+  width: 50px;
+  height: 50px;
+  margin: 20px auto;
+  border-radius: 50%;
+  border: 3px solid #ccc;
+  border-top: 3px solid #ffcc00;
+  animation: spin 0.5s linear infinite;
+}
 .navbar {
-  background-color: #FFCC00;
+  background-color: #ffcc00;
   color: #000;
   width: 100vw;
 
@@ -332,7 +404,7 @@ export default {
     a {
       font-weight: 600;
       text-decoration: none;
-      font-size: .9rem;
+      font-size: 0.9rem;
       color: #000;
 
       &:hover {
@@ -340,7 +412,6 @@ export default {
       }
     }
   }
-
 }
 .search-bar {
   border: 1px solid #222;
@@ -367,7 +438,37 @@ export default {
     height: 100%;
     background-color: transparent;
     &::placeholder {
-      font-size: .9rem;
+      font-size: 0.9rem;
+    }
+  }
+
+  .results {
+    position: absolute;
+    top: 100%;
+    animation: ease 0.5s fly-up;
+    width: 100%;
+    z-index: 2;
+    min-height: 100px;
+    max-height: 500px;
+    overflow-y: auto;
+    background-color: #000;
+    border-radius: 10px;
+    padding: 20px;
+    padding-top: 40px;
+
+    button {
+      position: absolute;
+      background: none;
+      border: none;
+      color: #fff;
+      top: 20px;
+      right: 20px;
+      width: fit-content;
+      outline: none;
+
+      i {
+        color: #fff;
+      }
     }
   }
 }
@@ -386,7 +487,7 @@ div.nav__items {
     margin: 0;
     width: 100%;
     position: fixed;
-    background-color: #FFCC00;
+    background-color: #ffcc00;
     height: calc(100vh - 60px);
     z-index: 1;
     top: 60px;
@@ -404,7 +505,7 @@ div.nav__items {
     }
 
     &.active {
-      animation: ease .7s fly-down forwards;
+      animation: ease 0.7s fly-down forwards;
     }
   }
 }
@@ -420,11 +521,11 @@ div.nav__items {
 .hamburger span {
   display: block;
   width: 100%;
-  box-shadow: 0 2px 10px 0 rgba(0,0,0,0.3);
+  box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.3);
   border-radius: 3px;
   height: 2px;
   background: #000;
-  transition: all .3s;
+  transition: all 0.3s;
   position: relative;
 }
 
@@ -433,27 +534,27 @@ span + span {
 }
 
 .hamburger.active span:nth-child(1) {
-  animation: ease .7s top forwards;
+  animation: ease 0.7s top forwards;
 }
 
 .hamburger.not-active span:nth-child(1) {
-  animation: ease .7s top-2 forwards;
+  animation: ease 0.7s top-2 forwards;
 }
 
 .hamburger.active span:nth-child(2) {
-  animation: ease .7s scaled forwards;
+  animation: ease 0.7s scaled forwards;
 }
 
 .hamburger.not-active span:nth-child(2) {
-  animation: ease .7s scaled-2 forwards;
+  animation: ease 0.7s scaled-2 forwards;
 }
 
 .hamburger.active span:nth-child(3) {
-  animation: ease .7s bottom forwards;
+  animation: ease 0.7s bottom forwards;
 }
 
 .hamburger.not-active span:nth-child(3) {
-  animation: ease .7s bottom-2 forwards;
+  animation: ease 0.7s bottom-2 forwards;
 }
 
 @keyframes top {
@@ -552,16 +653,16 @@ span + span {
 }
 
 ul.sub-menu {
-  background-color: #1D1D1D;
-  color: #FFF;
+  background-color: #1d1d1d;
+  color: #fff;
   position: absolute;
   border-radius: 10px;
-  animation: ease .5s fly-up;
+  animation: ease 0.5s fly-up;
   z-index: 2;
   li {
     text-align: left;
     padding: 20px;
-    border-bottom: 1px solid #FFF;
+    border-bottom: 1px solid #fff;
     &:first-child {
       border-radius: 10px 10px 0 0;
     }
@@ -572,11 +673,11 @@ ul.sub-menu {
     }
 
     &:hover {
-      background-color: rgba($color: #FFF, $alpha: 0.2);
+      background-color: rgba($color: #fff, $alpha: 0.2);
     }
 
     a {
-      color: #FFF;
+      color: #fff;
       white-space: nowrap;
     }
   }
@@ -588,10 +689,33 @@ ul.sub-menu {
   &--sub {
     left: 100%;
     top: 0;
-    animation: ease .5s fly-right;
+    animation: ease 0.5s fly-right;
     @media screen and (max-width: 992px) {
       display: none !important;
     }
   }
+}
+
+.gifs {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+
+  .gif {
+    height: 100px;
+    position: relative;
+    background-color: rgba($color: #fff, $alpha: 0.2);
+    border: 1px solid #000;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+
+.error {
+  font-size: .8rem;
+  font-weight: 600;
+  color: #FFF;
+  text-align: center;
 }
 </style>
